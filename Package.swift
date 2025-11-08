8bae4c1a20018383ac12b947389fe939d9e03d71
歆云 <dongkangxin.dkx@alibaba-inc.com>
2025-11-08 16:31:14 +0800

    first commit

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Mihomo",
-            url: "https://github.com/DongKangXin/mihomo/releases/download/iOS-1.0/Mihomo-iOS-Package.zip",
-            checksum: "b68298dc424ea5e88ba302849e84ff1d3abeffd9ba2dddf795fee851e751d6ab"
+            url: "https://github.com/DongKangXin/mihomo/releases/download/iOS-1.0/Mihomo.xcframework.zip",
+            checksum: "0a12841bc847ff6f1a1ed963cef202e95b6a122f2cda67af35be56de57d5d20e"
         )
     ]
 )
