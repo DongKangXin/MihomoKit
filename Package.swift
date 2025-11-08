414abeef77d432592c0c80607ced24785ebd85a8
歆云 <dongkangxin.dkx@alibaba-inc.com>
2025-11-08 16:38:41 +0800

    first commit

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "Mihomo",
+    name: "MihomoKit",
     platforms: [
         .iOS(.v14)
     ],
     products: [
-        .library(name: "Mihomo", targets: ["Mihomo"])
+        .library(name: "MihomoKit", targets: ["MihomoKit"])
     ],
     targets: [
         .binaryTarget(
-            name: "Mihomo",
+            name: "MihomoKit",
             url: "https://github.com/DongKangXin/mihomo/releases/download/iOS-1.0/Mihomo.xcframework.zip",
             checksum: "0a12841bc847ff6f1a1ed963cef202e95b6a122f2cda67af35be56de57d5d20e"
         )
